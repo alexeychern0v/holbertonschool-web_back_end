26f70674c58d24a844393260aa726796d0b484eb
Alexey Chernov <alexchernov063@gmail.com>
2024-04-25 12:11:26 +0200

    changed int to str in def

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,6 @@
 """ Type-annotated function to_kv """
 from typing import Tuple, Union
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[int, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Returns a tuple """
     return (k, v * v)
