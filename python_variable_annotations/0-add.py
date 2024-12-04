395d7fa21574ace700151decc799f831adebc31b
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-12-04 16:02:33 +0100

    Delete python_variable_annotations directory

--- python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" Type-annotated function add """
-
-
-def add(a: float, b: float) -> float:
-    """ Returns sum as a float """
-    return a + b
--- python_variable_annotations/7-to_kv.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" Type-annotated function to_kv """
-from typing import Tuple, Union
-
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """ Returns a tuple """
-    return (k, v * v)
