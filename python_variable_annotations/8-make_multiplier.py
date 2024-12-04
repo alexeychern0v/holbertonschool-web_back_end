519971fa91c5f9561d95713c6bcbad4ab4a23f25
alexeychern0v <alexchernov063@gmail.com>
2024-12-04 19:37:46 +0100

    8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
+from typing import Tuple, Union
+""" import List to precise what is inside of the list """
+
+def make_multiplier(multiplier: float)
