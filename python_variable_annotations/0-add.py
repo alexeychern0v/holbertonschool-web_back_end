c7d274ee607bde17e69a4a8e210c8d19e33a93d2
alexeychern0v <alexchernov063@gmail.com>
2024-12-04 16:33:58 +0100

    0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
+
+def add(a: float, b:float) -> float:
+    """ takes a and b floats and return their sum as a float """
+    return a + b
