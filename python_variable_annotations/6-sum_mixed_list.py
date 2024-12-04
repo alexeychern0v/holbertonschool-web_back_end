512f6660ecf4fc77df591ae989891829e24f5209
alexeychern0v <alexchernov063@gmail.com>
2024-12-04 17:50:33 +0100

    reduced length of def comment

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,5 +5,5 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """ takes a list mxd_lst of integers and floats and returns their sum as a float """
+    """ returns sum of mxt_lst list with ints and floats """
     return sum(mxd_lst)
