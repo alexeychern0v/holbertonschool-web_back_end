be6444383d5796400b046825e209e7b4282c416b
alexeychern0v <alexchernov063@gmail.com>
2024-12-04 17:28:49 +0100

    imported typing to use List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ variable annotations in Python """
+from typing import List
+""" import List to precise what is inside of the list"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ variable annotations in Python """
     return (sum(input_list))
