578081caae2c896259585a1c0fa0def78d62a448
alexeychern0v <alexchernov063@gmail.com>
2024-12-04 17:04:00 +0100

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
+
+
+def to_str(n: float) -> str:
+    """ takes n float and returns the string representation of n """
+    return str(n)
+
