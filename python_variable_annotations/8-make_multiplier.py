c4109e1e8e5a87486201fc995ef2db120aafd608
alexeychern0v <alexchernov063@gmail.com>
2024-12-05 07:32:19 +0100

    pycodestyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ variable annotations in Python """
 from typing import Callable
-""" import Callable to precise that make_multiplier 
+""" import Callable to precise that make_multiplier
 will return a function """
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """ takes a float multiplier and returns a function that 
+    """ takes a float multiplier and returns a function that
     multiplies a float by multiplier """
     def multiplier_func(m: float) -> float:
         """ defines m float and multiplies a float by multiplier """
