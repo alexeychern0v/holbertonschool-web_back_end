93163facc3869d3a83d19ea07adf7ee6ba32891e
alexeychern0v <alexchernov063@gmail.com>
2024-12-05 07:31:21 +0100

    8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 """ variable annotations in Python """
-from typing import Tuple, Union
-""" import List to precise what is inside of the list """
+from typing import Callable
+""" import Callable to precise that make_multiplier 
+will return a function """
 
-def make_multiplier(multiplier: float)
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ takes a float multiplier and returns a function that 
+    multiplies a float by multiplier """
+    def multiplier_func(m: float) -> float:
+        """ defines m float and multiplies a float by multiplier """
+        return m * multiplier
+    return multiplier_func
