bf70d9c46aedebd97721692f60f6578a7fca29a1
alexeychern0v <alexchernov063@gmail.com>
2024-12-12 14:26:06 +0100

    reduced lines' length

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -18,8 +18,10 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
+
     def __init__(self):
         self.__dataset = None
+
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -32,13 +34,16 @@ class Server:
 
         return self.__dataset
 
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """ find the correct indexes to paginate the dataset correctly
+            """ find the correct indexes to paginate the dataset
             and return the appropriate page of the dataset
             If the input arguments are out of range for the dataset
             an empty list should be returned """
-            assert isinstance(page, int) and page > 0, "page should be an int greater than 0"
-            assert isinstance(page_size, int) and page_size > 0, "page_size should be an int greater than 0"
+            assert isinstance(page, int) and page > 0, (
+                "page should be an int greater than 0")
+            assert isinstance(page_size, int) and page_size > 0, (
+                "page_size should be an int greater than 0")
             start_index, end_index = index_range(page, page_size)
             dataset = self.dataset()
             if start_index < len(dataset):
@@ -46,7 +51,9 @@ class Server:
             else:
                  return []
 
-    def get_hyper (self, page: int = 1, page_size: int = 10) -> Dict[str, Optional[int]]:
+
+    def get_hyper (self, page: int = 1, page_size: int = 10
+        ) -> Dict[str, Optional[int]]:
          """ returns a dict with different info"""
          data = self.get_page(page, page_size)
          total_lines = len(self.dataset())
