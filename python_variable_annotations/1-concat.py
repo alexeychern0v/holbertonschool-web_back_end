1cd3ad3d4e4670e8bb0cbedf5e48ce2d6aea26b3
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-04 13:22:47 +0200

    1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
 
+
+def concat(str1: str, str2: str) -> str:
+    """ takes a and b floats and return their sum as a float """
+    return str1 + str2
