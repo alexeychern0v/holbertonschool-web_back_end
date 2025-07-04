b555bf068cf8c6ff7ecf91e0e2bc9f66e6cd25dc
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-04 13:32:52 +0200

    both values annotated

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ variable annotations in Python """
-from typing import Tuple, Union
+from typing import Iterable, Sequence, List, Tuple
 """ import List to precise what is inside of the list """
 
-def element_length(lst):
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
