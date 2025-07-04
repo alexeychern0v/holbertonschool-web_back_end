543b911000a27587e262b77dec424e9a5beefd37
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-04 13:33:45 +0200

    added 1 blank line after comment

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,5 +3,6 @@
 from typing import Iterable, Sequence, List, Tuple
 """ import List to precise what is inside of the list """
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
