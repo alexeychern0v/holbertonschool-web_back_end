dd1c1fcf020cfa2a4ef763f3dfb64b3425a04fda
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-04 13:26:45 +0200

    5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
+from typing import List
+""" import List to precise what is inside of the list"""
 
+
+def sum_list(input_list: List[float]) -> float:
+    """ variable annotations in Python """
+    return (sum(input_list))
