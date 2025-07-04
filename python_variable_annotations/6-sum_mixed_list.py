f29ff1d707f54c94edc3d1563f77af249cb83925
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-04 13:27:43 +0200

    6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
+from typing import List, Union
+""" import List to precise what is inside of the list """
 
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ returns sum of mxt_lst list with ints and floats """
+    return sum(mxd_lst)
