7e0b84e0c731b86f02f3ef2696cd48b67d5f1edf
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-04 13:28:25 +0200

    7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
+from typing import Tuple, Union
+""" import List to precise what is inside of the list """
 
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Takes a string k and an int OR float v as arguments and returns a tuple
+    The first element of the tuple is the string k
+    The 2nd element is the square of v and should be annotated as a float
+    """
+    return (k, float(v**2))
