3a3adf5feca4e792cd5a640a908c9155ded988fd
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-04 13:23:57 +0200

    2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+""" variable annotations in Python """
+import math
+""" imported module math for floor"""
 
+
+def floor(n: float) -> int:
+    """ takes n float and returns the floor n"""
+    return math.floor(n)
